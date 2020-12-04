0a6bb4e3bdcbb6e1d8e2e1b243a2bdcac100db3b
SimonMcDonnell <smcdonnell1049@gmail.com>
2020-12-03 20:46:59 -0500

    refactor tensor.py to implement backwards function within each class method

--- mytorch/tensor.py
+++ mytorch/tensor.py
@@ -1,18 +1,43 @@
 import numpy as np
-from mytorch.function import Function
 
 
 class Tensor:
-    def __init__(self, data, grad=None, grad_fn=None, is_leaf=True,
-                 requires_grad=False):
+    def __init__(self, data: np.ndarray, prev: tuple = ()) -> None:
         self.data = data
-        self.grad = grad
-        self.grad_fn = grad_fn
-        self.is_leaf = is_leaf
-        self.requires_grad = requires_grad
+        self.prev = set(prev)
+        self.grad = None
+        self.grad_fn = None
 
     @property
-    def shape(self):
+    def shape(self) -> tuple:
         return self.data.shape
 
-    def backward(self):
+    @property
+    def T(self) -> 'Tensor':
+        def _backward():
+            self.grad = out.grad.T
+
+        out = Tensor(self.data.T, (self, None))
+        out.grad_fn = _backward
+        return out
+
+    def __repr__(self) -> str:
+        return f"Tensor(data={self.data}, grad={self.grad})"
+
+    def __add__(self, b: 'Tensor') -> 'Tensor':
+        def _backward():
+            self.grad = out.grad
+            b.grad = out.grad
+
+        out = Tensor(self.data + b.data, (self, b))
+        out.grad_fn = _backward
+        return out
+
+    def dot(self, b: 'Tensor') -> 'Tensor':
+        def _backward():
+            self.grad = out.grad.dot(b.T)
+            b.grad = self.grad.T.dot(out.grad)
+
+        out = Tensor(self.data.dot(b.data), (self, b))
+        out.grad_fn = _backward
+        return out
