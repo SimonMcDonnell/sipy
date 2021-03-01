a85c65c5a13e1043a0a1185852a0508b3bfceed3
SimonMcDonnell <smcdonnell1049@gmail.com>
2021-03-01 16:00:50 -0500

    add momentum to SGD

--- mytorch/optim.py
+++ mytorch/optim.py
@@ -4,14 +4,21 @@ import numpy as np
 
 
 class SGD:
-    def __init__(self, parameters: List['Tensor'], lr: float = 0.01) -> None:
+    # SGD with momentum
+    def __init__(self, parameters: List['Tensor'], lr: float = 0.01,
+                 gamma: float = 0.9) -> None:
         self.parameters = parameters
         self.lr = lr
+        self.gamma = gamma
+        self.v_prev = [np.zeros(param.shape) for param in self.parameters]
 
     def zero_grad(self) -> None:
         for param in self.parameters:
             param.grad = 0.0
 
     def step(self) -> None:
-        for param in self.parameters:
-            param.data = param.data - self.lr * param.grad
+        for i in range(len(self.parameters)):
+            v_t = (self.gamma * self.v_prev[i]) + \
+                (self.lr * self.parameters[i].grad)
+            self.parameters[i].data = self.parameters[i].data - v_t
+            self.v_prev[i] = v_t
